26f7e9886144d0d08708db05c79f060686099cdf
jackjaw <shdongwen@gmail.com>
2022-03-29 11:59:52 +0800

    update scripts

---
+++ deploy/data.sh
@@ -0,0 +1,36 @@
+# Load EVE dev resources
+echo "downloading sde.zip from remote"
+curl -O https://eve-static-data-export.s3-eu-west-1.amazonaws.com/tranquility/sde.zip
+
+echo "unzip sde.zip"
+unzip sde.zip >/dev/null 2>&1
+
+num=$(ls | grep .yaml | wc -l)
+if [ $num -gt 0 ]; then
+        echo "cleanup the old yaml files"
+        rm *.yaml
+fi
+
+echo "save the yaml files"
+mv sde/fsd/typeIDs.yaml .
+mv sde/fsd/factions.yaml .
+mv sde/fsd/npcCorporations.yaml .
+
+echo "load the yaml files completed"
+rm -rf sde
+rm sde.zip
+
+# Load Fuzzwork latest resources
+num=$(ls | grep .csv | wc -l)
+if [ $num -gt 0 ]; then
+        echo "cleanup the old csv files"
+        rm *.csv
+fi
+
+echo "loading csv files from remote"
+curl -O https://www.fuzzwork.co.uk/dump/latest/industryActivityProducts.csv
+curl -O https://www.fuzzwork.co.uk/dump/latest/industryActivityMaterials.csv
+
+echo "load the csv files completed"
+
+ls -lh
--- deploy/stop.sh
+++ deploy/stop.sh
@@ -2,3 +2,9 @@ pid=$(ps -ef | grep evelp | grep -v grep | awk '{print $2}')
 if [ -n "$pid" ]; then
         kill -9 $pid;
 fi
+
+if [$? -eq 0 ];then
+        echo "stopped"
+else
+        echo "failed to stop"
+fi
